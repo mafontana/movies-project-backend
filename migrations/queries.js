7e5da0668bde007fcb0bce29090430ba30aa5b88
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-12-06 11:55:10 -0700

    created queries.js file with filler/example code for queries

---
+++ migrations/queries.js
@@ -0,0 +1,19 @@
+module.exports = {
+    listAll(){
+        return db('students')
+    },
+    getById(id) {
+        return db('students').where('id', id)
+    }, 
+    createStudent(newStudent) {
+        return db('students').insert(newStudent).returning('*')
+    },
+    deleteStudent(id){
+        return db('students')
+            .where('id', id)
+            .delete()
+    },
+    updateStudent(id, student) {
+        return db('students').where('id', id).update(student)
+    }
+}
